0e7387961f2cc54f6efa3b6ececf701c354dc0ee
satviksinha <f20200993@pilani.bits-pilani.ac.in>
2021-06-12 08:57:46 +0530

    touch scroll added

--- src/components/Road/Road.js
+++ src/components/Road/Road.js
@@ -1,81 +1,75 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './Road.css';
-import Doodle from '../Doodle/Doodle';
-import Milestone from '../Milestone/Milestone';
-import CustomModal from '../Modal/Modal';
-import Landing from '../LandingPage/Landing';
-import Rotate from '../Rotate/Rotate'
-import BackgroundImage from '../BackgroundImage/BackgroundImage'
+import React, { useState, useEffect, useRef } from "react";
+import "./Road.css";
+import Doodle from "../Doodle/Doodle";
+import Milestone from "../Milestone/Milestone";
+import CustomModal from "../Modal/Modal";
+import Landing from "../LandingPage/Landing";
+import Rotate from "../Rotate/Rotate";
+import BackgroundImage from "../BackgroundImage/BackgroundImage";
 /*
 Slower the scroll speed
 */
 
 function Road() {
-    
+  const [position, setPosition] = useState(0);
 
-    const [position, setPosition] = useState(0);
+  const speed = 1;
+  const speed_2 = 2;
 
-    const speed = 1;
+  const onWheel = (e) => {
+    e.preventDefault();
+    const container = scrollRef.current;
+    const containerScrollPosition = scrollRef.current.scrollLeft;
 
-    const onWheel = (e) => {
-        e.preventDefault();
-        const container = scrollRef.current;
-        const containerScrollPosition = scrollRef.current.scrollLeft;
+    container.scrollTo({
+      top: 0,
+      left: containerScrollPosition + e.deltaY * speed,
+    });
 
-        container.scrollTo({
-            top: 0,
-            left: containerScrollPosition + e.deltaY*speed
-        });
-
-        if ((position + e.deltaY*1.1*speed > 15) && (position+e.deltaY*1.1*speed < 5900)) {
-            setPosition(position + e.deltaY*1.1*speed);
-        }
-        
-    };
-
-    function useEvent(event, handler){
-        useEffect(() => {
-            window.addEventListener(event, handler);
-            return function cleanup() {
-                window.removeEventListener(event, handler);
-            }
-        } )
+    if (
+      position + e.deltaY * 1.1 * speed > 15 &&
+      position + e.deltaY * 1.1 * speed < 5900
+    ) {
+      setPosition(position + e.deltaY * 1.1 * speed);
     }
-    
-    // const moveRight = (e) => {
-    //     e.preventDefault();
-    //     if (e.key === ' ') {
-    //         setPosition(position + 50);
-    //     }
-    // }
+  };
 
-    // useEvent('keyup', moveRight);
+  function useEvent(event, handler) {
+    useEffect(() => {
+      window.addEventListener(event, handler);
+      return function cleanup() {
+        window.removeEventListener(event, handler);
+      };
+    });
+  }
 
-    
+  // const moveRight = (e) => {
+  //     e.preventDefault();
+  //     if (e.key === ' ') {
+  //         setPosition(position + 50);
+  //     }
+  // }
 
-    const scrollRef = useRef(null);
+  // useEvent('keyup', moveRight);
 
-    
-   
+  const scrollRef = useRef(null);
 
-    const milestones = ['First Year', 'Second Year', 'Third Year', 'Fourth Year'];
+  const milestones = ["First Year", "Second Year", "Third Year", "Fourth Year"];
 
+  return (
+    <div>
+      <Rotate />
+      <BackgroundImage />
+      <div className="roadContainer">
+        <div ref={scrollRef} className="road" onWheel={onWheel}>
+          <Doodle id="moving_doodle" move={position} />
 
-  return (<div>
-        <Rotate/>
-        <BackgroundImage/>
-          <div className="roadContainer">
-      <div
-        ref={scrollRef}
-        className="road"
-        onWheel={onWheel}
-      > 
-              <Doodle id="moving_doodle" move={position}/>
-            
-              {milestones.map( (milestone, index) => <Milestone left={1250*(index+1)} milestone={milestone} />)}
-              </div>
-              </div>
-              </div>
+          {milestones.map((milestone, index) => (
+            <Milestone left={1250 * (index + 1)} milestone={milestone} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/Seniors/Abhijeet/AbhijeetRoad.js
+++ src/components/Seniors/Abhijeet/AbhijeetRoad.js
@@ -1,87 +1,192 @@
-import React, { useState, useEffect, useRef } from 'react';
-import '../../Road/Road.css';
-import Doodle from '../../Doodle/Doodle';
-import Milestone from '../../Milestone/Milestone';
-import CustomModal from '../../Modal/Modal';
-import Landing from '../../LandingPage/Landing';
-import Rotate from '../../Rotate/Rotate'
-import BackgroundImage from '../../BackgroundImage/BackgroundImage'
+import React, { useState, useEffect, useRef } from "react";
+import "../../Road/Road.css";
+import Doodle from "../../Doodle/Doodle";
+import Milestone from "../../Milestone/Milestone";
+import CustomModal from "../../Modal/Modal";
+import Landing from "../../LandingPage/Landing";
+import Rotate from "../../Rotate/Rotate";
+import BackgroundImage from "../../BackgroundImage/BackgroundImage";
 
 function AbhijeetRoad() {
-    
+  const [position, setPosition] = useState(0);
 
-    const [position, setPosition] = useState(0);
+  const [modalShown, setModalShown] = useState(false);
 
-    const [modalShown, setModalShown] = useState(false);
+  function handleClose() {
+    setModalShown(false);
+  }
 
-    function handleClose() {
-        
-        setModalShown(false);
-    }
+  function handleShow() {
+    setModalShown(true);
+  }
 
-    function handleShow() {
-        setModalShown(true);
-    }
+  const speed = 20; //set the speed here.
+  const speed_2 = 2;
 
-    const speed = 0.4;  //set the speed here.
+  const moveRight = (e) => {
+    e.preventDefault();
+    if (e.key === "ArrowRight") {
+      if (!modalShown) {
+        const container = scrollRef.current;
+        const containerScrollPosition = scrollRef.current.scrollLeft;
 
-    const onWheel = (e) => {
+        container.scrollTo({
+          top: 0,
+          left: containerScrollPosition + 10 * speed_2,
+        });
 
-        if (!modalShown) {
-            const container = scrollRef.current;
-            const containerScrollPosition = scrollRef.current.scrollLeft;
-
-            container.scrollTo({
-                top: 0,
-                left: containerScrollPosition + e.deltaY*speed
-            });
-
-            if ((position + e.deltaY*1.1*speed > 15) && (position+e.deltaY*1.1*speed < 5900)) {
-                setPosition(position + e.deltaY*1.1*speed);
-            }
-            
+        if (
+          position + 10 * 1.1 * speed_2 > 15 &&
+          position + 10 * 1.1 * speed_2 < 5900
+        ) {
+          setPosition(position + 10 * 1.1 * speed_2);
         }
-    };
-
-    function useEvent(event, handler){
-        useEffect(() => {
-            window.addEventListener(event, handler);
-            return function cleanup() {
-                window.removeEventListener(event, handler);
-            }
-        } )
+      }
     }
-    
-    // const moveRight = (e) => {
-    //     e.preventDefault();
-    //     if (e.key === ' ') {
-    //         setPosition(position + 50);
-    //     }
-    // }
-    // FOR IMPLEMENTING ANY ACTIONS OTHER THAN MOVING USE THIS
-    // useEvent('keyup', moveRight);
+  };
 
-    
+  const moveLeft = (e) => {
+    e.preventDefault();
+    if (e.key === "ArrowLeft") {
+      if (!modalShown) {
+        const container = scrollRef.current;
+        const containerScrollPosition = scrollRef.current.scrollLeft;
 
-    const scrollRef = useRef(null);
+        container.scrollTo({
+          top: 0,
+          left: containerScrollPosition - 10 * speed_2,
+        });
 
-    const milestones = ['First Year', 'Second Year', 'Third Year', 'Fourth Year'];
+        if (
+          position - 10 * 1.1 * speed_2 > 30 &&
+          position - 10 * 1.1 * speed_2 < 5900
+        ) {
+          setPosition(position - 10 * 1.1 * speed_2);
+        }
+      }
+    }
+  };
 
+  useEvent("keydown", moveRight);
+  useEvent("keydown", moveLeft);
+  let start = 0,
+    end = 0;
 
-  return (<div>
-        <Rotate/>
-        <BackgroundImage/>
-          <div className="roadContainer">
-      <div
-        ref={scrollRef}
-        className="road"
-        onWheel={onWheel}
-      > 
-              <Doodle id="moving_doodle" move={position}/>
-              {milestones.map((milestone, index) => <Milestone left={1250 * (index + 1)} milestone={milestone} showModal={modalShown} handleClose={handleClose} handleShow={handleShow}/>)}
-              </div>
-              </div>
-              </div>
+  const onTouchStart = (e) => {
+    start = e.touches[0].clientX;
+  };
+
+  const onTouchEnd = (e) => {
+    // console.log(e.touches[0].clientX);
+  };
+
+  const onTouchMove = (e) => {
+    end = e.touches[0].clientX;
+    if (end < start) {
+      if (!modalShown) {
+        const container = scrollRef.current;
+        const containerScrollPosition = scrollRef.current.scrollLeft;
+
+        container.scrollTo({
+          top: 0,
+          left: containerScrollPosition + (start - end) * speed,
+        });
+
+        if (
+          position + (start - end) * 1.1 * speed > 15 &&
+          position + (start - end) * 1.1 * speed < 5900
+        ) {
+          setPosition(position + (start - end) * 1.1 * speed);
+        }
+      }
+    }
+    if (end > start) {
+      if (!modalShown) {
+        const container = scrollRef.current;
+        const containerScrollPosition = scrollRef.current.scrollLeft;
+
+        container.scrollTo({
+          top: 0,
+          left: containerScrollPosition - (end - start) * speed,
+        });
+
+        if (
+          position - (end - start) * 1.1 * speed > 15 &&
+          position - (end - start) * 1.1 * speed < 5900
+        ) {
+          setPosition(position - (end - start) * 1.1 * speed);
+        }
+      }
+    }
+  };
+
+  const onWheel = (e) => {
+    if (!modalShown) {
+      const container = scrollRef.current;
+      const containerScrollPosition = scrollRef.current.scrollLeft;
+
+      container.scrollTo({
+        top: 0,
+        left: containerScrollPosition + e.deltaY * speed,
+      });
+
+      if (
+        position + e.deltaY * 1.1 * speed > 15 &&
+        position + e.deltaY * 1.1 * speed < 5900
+      ) {
+        setPosition(position + e.deltaY * 1.1 * speed);
+      }
+    }
+  };
+
+  function useEvent(event, handler) {
+    useEffect(() => {
+      window.addEventListener(event, handler);
+      return function cleanup() {
+        window.removeEventListener(event, handler);
+      };
+    });
+  }
+
+  // const moveRight = (e) => {
+  //     e.preventDefault();
+  //     if (e.key === ' ') {
+  //         setPosition(position + 50);
+  //     }
+  // }
+  // FOR IMPLEMENTING ANY ACTIONS OTHER THAN MOVING USE THIS
+  // useEvent('keyup', moveRight);
+
+  const scrollRef = useRef(null);
+
+  const milestones = ["First Year", "Second Year", "Third Year", "Fourth Year"];
+
+  return (
+    <div>
+      <Rotate />
+      <BackgroundImage />
+      <div className="roadContainer">
+        <div
+          ref={scrollRef}
+          className="road"
+          onWheel={onWheel}
+          onTouchMove={onTouchMove}
+          onTouchStart={onTouchStart}
+          onTouchEnd={onTouchEnd}
+        >
+          <Doodle id="moving_doodle" move={position} />
+          {milestones.map((milestone, index) => (
+            <Milestone
+              left={1250 * (index + 1)}
+              milestone={milestone}
+              showModal={modalShown}
+              handleClose={handleClose}
+              handleShow={handleShow}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
